c16ef587ae390a497d27326fee7e24f8ca1d0156
glennc <glennc@microsoft.com>
2017-06-12 18:57:17 -0700

    Switch to smaller meta-package.

--- sample/Empty/.net/Startup.cs
+++ sample/Empty/.net/Startup.cs
@@ -16,7 +16,8 @@ namespace Empty
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvcCore()
+                    .AddJsonFormatters();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
